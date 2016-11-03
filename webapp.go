70c8deaf0bcbb7cc7387f593db41967e810a6329
Chrissweir <christopher.weir@hotmail.com>
2016-11-03 15:27:06 +0000

    Sending fils to mongodb

--- webapp.go
+++ webapp.go
@@ -11,59 +11,53 @@ import (
 func main() {
 	m := macaron.Classic()
 	m.Use(macaron.Renderer())
-	m.Get("/upload", uploadhandler)
+	m.Post("/", upload)
 	m.Run()
 }
-func uploadhandler(w http.ResponseWriter, req *http.Request) {
+
+func upload(req *http.Request) {
+	fmt.Println("Uploadhandler start")
 	session, err := mgo.Dial("127.0.0.1:27017")
 	if err != nil {
 		panic(err)
 	}
 
 	defer session.Close()
-
-	// Capture multipart form file information
+// Adapted from: http://stackoverflow.com/questions/22159665/store-uploaded-file-in-mongodb-gridfs-using-mgo-without-saving-to-memory
+	// Retrieve the form file 
 	file, handler, err := req.FormFile("uploadfile")
+	//Check if there is an error
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Read the file into memory
+	data, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// Read the file into memory
-	data, err := ioutil.ReadAll(file)
-	// ... check err value for nil
-
 	// Specify the Mongodb database
 	my_db := session.DB("Images")
+	// Set the filename as the uploadfile name
 	filename := handler.Filename
 
 	// Create the file in the Mongodb Gridfs instance
 	my_file, err := my_db.GridFS("fs").Create(filename)
-	// ... check err value for nil
-
+	if err != nil {
+		fmt.Println(err)
+	}
 	// Write the file to the Mongodb Gridfs instance
 	n, err := my_file.Write(data)
-	// ... check err value for nil
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Close the file
 	err = my_file.Close()
-	// ... check err value for nil
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Write a log type message
 	fmt.Printf("%d bytes written to the Mongodb instance\n", n)
-
-	// ... other statements redirecting to rest of user flow...
 }
- /*func uploadhandler(r *http.Request){
-
-	 session, err := mgo.Dial("127.0.0.1:27017")
-	 if err != nil {
-		 panic(err)
-	 }
-
-	 defer session.Close()
-	 db := session.DB("Images")
-
-	 file, header, err:= r.FormFile("uploadfile") // the input file by form
-	 defer file.Close()
-
- }*/
